aa90100a8f13db87b379583103444493f0e76abc
FootGodHelpHackingPurge <hitlersecuencia@gmail.com>
2020-01-31 13:46:39 -0300

    Oro Joven

---
+++ install.sh
@@ -0,0 +1,18 @@
+echo -e "\033[92mInstall OroJoven\033[0m"
+sleep 2
+apt install fish -y
+apt install git -y
+apt install python python2 -y
+apt install proot -y
+apt install figlet -y
+apt install ruby -y
+gem install lolcat
+sleep 2
+rm $PREFIX/etc/fish/config.fish
+rm $PREFIX/etc/bash.bashrc
+rm $PREFIX/etc/motd
+cd .f 
+mv config.fish $PREFIX/etc/fish
+mv bash.bashrc $PREFIX/etc/
+exit
+
